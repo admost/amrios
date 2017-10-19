bd90d807ff9c8a0574758315cd931b13558fcecc
Mehmet Karagöz <mehmetkaragoz@mail.com>
2017-10-19 11:10:07 +0300

    1.3.12 released.

--- AMRDemo/AMRDemo/AppDelegate.m
+++ AMRDemo/AMRDemo/AppDelegate.m
@@ -32,7 +32,7 @@
     [AMRSDK startTestSuiteWithZones:@[@"b4009772-de04-42c4-bbaa-c18da9e4a1ab",
                                       @"39f74377-5682-436a-9338-9d1c4df410bd",
                                       @"2bdefd44-5269-4cbc-b93a-373b74a2f067"]];
-
+    
     return YES;
 }
 
