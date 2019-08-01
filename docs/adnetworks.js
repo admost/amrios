9d9a71dcd3c25d38e111402411603d92b1750385
hanilozmen <hanilozmen@gmail.com>
2019-08-01 14:13:33 +0300

    ad network file moved to a single file to be accessible from unity

--- docs/adnetworks.js
+++ docs/adnetworks.js
@@ -1,47 +1,15 @@
-var obj = JSON.parse(`
-{"ad_networks":
-[{"name":"AMR","status":true,"pod":"AMRSDK","version":"1.4", "minTargetSdk":"8.0"},
-{"name":"AdColony","status":false,"pod":"AMRAdapterAdcolony","version":"3.3", "minTargetSdk":"8.0"},
-{"name":"AdMob","status":false,"pod":"AMRAdapterAdmob","version":"7.47", "minTargetSdk":"8.0"},
-{"name":"Admost","status":false,"pod":"AMRAdapterAdmost","version":"1.2", "minTargetSdk":"8.0"},
-{"name":"Adtiming","status":false,"pod":"AMRAdapterAdtiming","version":"3.6", "minTargetSdk":"8.0"},
-{"name":"Amazon","status":false,"pod":"AMRAdapterAmazon","version":"2.2", "minTargetSdk":"8.0"},
-{"name":"AppLovin","status":false,"pod":"AMRAdapterApplovin","version":"6.8", "minTargetSdk":"9.0"},
-{"name":"Appnext","status":false,"pod":"AMRAdapterAppnext","version":"1.9", "minTargetSdk":"8.0"},
-{"name":"Appsamurai","status":false,"pod":"AMRAdapterAppsamurai","version":"1.4", "minTargetSdk":"8.0"},
-{"name":"Chartboost","status":false,"pod":"AMRAdapterChartboost","version":"7.5", "minTargetSdk":"8.0"},
-{"name":"DFP","status":false,"pod":"AMRAdapterDFP","version":"7.47", "minTargetSdk":"8.0"},
-{"name":"Facebook","status":false,"pod":"AMRAdapterFacebook","version":"5.4", "minTargetSdk":"9.0"},
-{"name":"Fyber","status":false,"pod":"AMRAdapterFyber","version":"8.22", "minTargetSdk":"8.0"},
-{"name":"InMobi","status":false,"pod":"AMRAdapterInmobi","version":"7.3", "minTargetSdk":"9.0"},
-{"name":"Inneractive","status":false,"pod":"AMRAdapterInneractive","version":"7.3", "minTargetSdk":"8.0"},
-{"name":"IronSource","status":false,"pod":"AMRAdapterIronsource","version":"6.8", "minTargetSdk":"8.0"},
-{"name":"Mintegral","status":false,"pod":"AMRAdapterMintegral","version":"5.5", "minTargetSdk":"8.0"},
-{"name":"Mobfox","status":false,"pod":"AMRAdapterMobfox","version":"3.6", "minTargetSdk":"9.0"},
-{"name":"Mopub","status":false,"pod":"AMRAdapterMopub","version":"5.7", "minTargetSdk":"9.0"},
-{"name":"MyTarget","status":false,"pod":"AMRAdapterMytarget","version":"5.3", "minTargetSdk":"8.0"},
-{"name":"Nexage(AOL)","status":false,"pod":"AMRAdapterNexage","version":"6.8", "minTargetSdk":"8.0"},
-{"name":"Pollfish","status":false,"pod":"AMRAdapterPollfish","version":"5.0", "minTargetSdk":"8.0"},
-{"name":"Smaato","status":false,"pod":"AMRAdapterSmaato","version":"10.2", "minTargetSdk":"8.0"},
-{"name":"StartApp","status":false,"pod":"AMRAdapterStartapp","version":"3.11", "minTargetSdk":"8.0"},
-{"name":"Tapjoy","status":false,"pod":"AMRAdapterTapjoy","version":"12.3", "minTargetSdk":"8.0"},
-{"name":"TapResearch","status":false,"pod":"AMRAdapterTapresearch","version":"2.0", "minTargetSdk":"8.0"},
-{"name":"UnityAds","status":false,"pod":"AMRAdapterUnity","version":"3.2", "minTargetSdk":"8.0"},
-{"name":"Vungle","status":false,"pod":"AMRAdapterVungle","version":"6.3", "minTargetSdk":"8.0"},
-{"name":"YandexAds","status":false,"pod":"AMRAdapterYandex","version":"2.13", "minTargetSdk":"8.0"},
-{"name":"YouAppi","status":false,"pod":"AMRAdapterYouappi","version":"4.4", "minTargetSdk":"9.0"}]}`
-);
-
+var obj;
 var htmlString = "";
-
+var htmlPodFile = "";
 var arrayAppGradlePackages = [];
 
+function amrInitPage(adNetworkJson) {
+  obj = adNetworkJson;
+  fillAdNetworkList(obj);
+  htmlPodFile = "source \'https:\/\/github.com\/CocoaPods\/Specs.git\'\r\nplatform :ios, \'8.0\'\r\n\r\nuse_frameworks!\r\n\r\ntarget \'MyAwesomeTarget\' do\r\n#core SDK\r\npod \'AMRSDK\', \'~&gt; 1.2\'\r\n#mediation adapters\n";
+  fillPodFileCode();
 
-fillAdNetworkList(obj);
-
-var htmlPodFile = "source \'https:\/\/github.com\/CocoaPods\/Specs.git\'\r\nplatform :ios, \'8.0\'\r\n\r\nuse_frameworks!\r\n\r\ntarget \'MyAwesomeTarget\' do\r\n#core SDK\r\npod \'AMRSDK\', \'~&gt; 1.2\'\r\n#mediation adapters\n";
-
-fillPodFileCode();
+}
 
 
 function toggleAdNetworkStatus(adNetworkName) {
