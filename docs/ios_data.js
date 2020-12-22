1a7cae1960210b072043d877e05958e07dea255d
Mehmet Karagöz <eng.mkaragoz@gmail.com>
2020-12-22 12:22:46 +0300

    iOS keys added.

--- docs/ios_data.js
+++ docs/ios_data.js
@@ -38,3 +38,19 @@ function getIosNetworks() {
   {"name":"YandexAds","status":false,"pod":"AMRAdapterYandex","version":"2.20", "minTargetSdk":"8.0"},
   {"name":"YouAppi","status":false,"pod":"AMRAdapterYouappi","version":"5.0", "minTargetSdk":"9.0"}]}`);
 }
+
+function getIos14Keys() {
+  return JSON.parse(`
+  [
+    {"name":"AdColony","skAdNetwork": ["4PFYVQ9L8R.skadnetwork","YCLNXRL5PM.skadnetwork","V72QYCH5UU.skadnetwork","TL55SBB4FM.skadnetwork","T38B2KH725.skadnetwork","PRCB7NJMU6.skadnetwork","PPXM28T8AP.skadnetwork","MLMMFZH3R3.skadnetwork","KLF5C3L5U5.skadnetwork","HS6BDUKANM.skadnetwork","C6K4G5QG8M.skadnetwork","9T245VHMPL.skadnetwork","9RD848Q2BZ.skadnetwork","8S468MFL3Y.skadnetwork","7UG5ZH24HU.skadnetwork","4FZDC2EVR5.skadnetwork","4468KM3ULZ.skadnetwork","3RD42EKR43.skadnetwork","2U9PT9HC89.skadnetwork","M8DBW4SV7C.skadnetwork","7RZ58N8NTL.skadnetwork","EJVT5QM6AK.skadnetwork","5LM9LJ6JB7.skadnetwork","44JX6755AQ.skadnetwork", "MTKV5XTK9E.skadnetwork"]},
+    {"name":"AdMob","skAdNetwork": ["cstr6suwn9.skadnetwork"]},
+    {"name":"Adtiming","skAdNetwork": ["488r3q3dtq.skadnetwork"]},
+    {"name":"AppLovin","skAdNetwork": ["2u9pt9hc89.skadnetwork","4468km3ulz.skadnetwork","4fzdc2evr5.skadnetwork","7ug5zh24hu.skadnetwork","8s468mfl3y.skadnetwork","9rd848q2bz.skadnetwork","9t245vhmpl.skadnetwork","av6w8kgt66.skadnetwork","f38h382jlk.skadnetwork","hs6bdukanm.skadnetwork","kbd757ywx3.skadnetwork","ludvb6z3bs.skadnetwork","m8dbw4sv7c.skadnetwork","mlmmfzh3r3.skadnetwork","prcb7njmu6.skadnetwork","t38b2kh725.skadnetwork","tl55sbb4fm.skadnetwork","wzmmz9fp6w.skadnetwork","yclnxrl5pm.skadnetwork","ydx93a7ass.skadnetwork"]},
+    {"name":"Chartboost","skAdNetwork": ["F38H382JLK.skadnetwork"]},
+    {"name":"Criteo","skAdNetwork": ["hs6bdukanm.skadnetwork"]},
+    {"name":"Facebook","skAdNetwork": ["v9wttpbfk9.skadnetwork", "n38lu8286q.skadnetwork"]},
+    {"name":"HyprMx","skAdNetwork": ["NU4557A4JE.skadnetwork"]}
+  ]
+    `);
+  }
+}
