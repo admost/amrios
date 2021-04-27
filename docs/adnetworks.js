8a80e2a82c5a77b99494523e289e2820d1c682ea
Ezgi Üstünel <ezgiustunel97@gmail.com>
2021-04-27 14:53:36 +0300

    ios_data.js removed

--- docs/adnetworks.js
+++ docs/adnetworks.js
@@ -4,19 +4,23 @@ var htmlString = "";
 var htmlPodFile = "";
 var arrayAppGradlePackages = [];
 
-function amrInitPage(adNetworkJson, ios14json) {
-  obj = adNetworkJson;
-  obj14 = ios14json;
+function amrInitPage() {
+  //obj = adNetworkJson;
+  //obj14 = ios14json;
+  response = httpGet("https://admost.github.io/amrios/adnetwork.json");
+  obj = getNetworks(response);
   fillAdNetworkList(obj);
   htmlPodFile = "source \'https:\/\/github.com\/CocoaPods\/Specs.git\'\r\nplatform :ios, \'9.0\'\r\n\r\nuse_frameworks!\r\n\r\ntarget \'MyAwesomeTarget\' do\r\n#core SDK\r\npod \'AMRSDK\', \'~&gt; 1.2\'\r\n#mediation adapters\n";
   fillPodFileCode();
-
 }
-
+   
+function getNetworks(obj){
+    return JSON.parse(obj);
+}
 
 function toggleAdNetworkStatus(adNetworkName) {
     var i = getPositionOfAdNetworkOnJSONArray(adNetworkName);
-    if (obj.ad_networks[i].status == true) {
+    if (obj.adNetworks[i].status == true) {
         removeAdNetworkToPodFile(i);
     } else {
         addAdNetworkToPodFile(i);
@@ -24,22 +28,22 @@ function toggleAdNetworkStatus(adNetworkName) {
 }
 
 function addAdNetworkToPodFile(i) {
-    obj.ad_networks[i].status = true;
+    obj.adNetworks[i].status = true;
     fillAdNetworkList(obj);
     fillPodFileCode();
     fillIos14FileCode();
 }
 
 function removeAdNetworkToPodFile(i) {
-    obj.ad_networks[i].status = false;
+    obj.adNetworks[i].status = false;
     fillAdNetworkList(obj);
     fillPodFileCode();
     fillIos14FileCode();
 }
 
 function getPositionOfAdNetworkOnJSONArray(adNetworkName) {
-    for (var i = 0; i < obj.ad_networks.length; i++) {
-        if (obj.ad_networks[i].name === adNetworkName) {
+    for (var i = 0; i < obj.adNetworks.length; i++) {
+        if (obj.adNetworks[i].displayName === adNetworkName) {
             return i;
         }
     }
@@ -48,13 +52,13 @@ function getPositionOfAdNetworkOnJSONArray(adNetworkName) {
 
 function fillAdNetworkList() {
     htmlString = '<div class="btn-group" style="margin:8px;" id="btn-group-network-AMR"> <button type="button" id="btn-name-network-AMR" class="btn btn-success">AMR</button>  </div>&nbsp;';
-    for (var i = 1; i < obj.ad_networks.length; i++) {
-        htmlString = htmlString + '<div class="btn-group" style="margin:8px; id="btn-group-network-' + obj.ad_networks[i].name + '"> <button type="button" id="btn-name-network-' + obj.ad_networks[i].name + '" class="btn btn-default">' + obj.ad_networks[i].name + '</button> <button type="button" id="btn-icon-network-' + obj.ad_networks[i].name + '" onclick="toggleAdNetworkStatus(\'' + obj.ad_networks[i].name + '\');" class="btn btn-';
+    for (var i = 1; i < obj.adNetworks.length; i++) {
+        htmlString = htmlString + '<div class="btn-group" style="margin:8px; id="btn-group-network-' + obj.adNetworks[i].displayName + '"> <button type="button" id="btn-name-network-' + obj.adNetworks[i].displayName + '" class="btn btn-default">' + obj.adNetworks[i].displayName + '</button> <button type="button" id="btn-icon-network-' + obj.adNetworks[i].displayName + '" onclick="toggleAdNetworkStatus(\'' + obj.adNetworks[i].displayName + '\');" class="btn btn-';
 
-        obj.ad_networks[i].status ? htmlString = htmlString + "danger" : htmlString = htmlString + "success";
+        obj.adNetworks[i].status ? htmlString = htmlString + "danger" : htmlString = htmlString + "success";
         htmlString = htmlString + ' dropdown-toggle"> <span class="fa fa-';
 
-        obj.ad_networks[i].status ? htmlString = htmlString + "minus" : htmlString = htmlString + "plus";
+        obj.adNetworks[i].status ? htmlString = htmlString + "minus" : htmlString = htmlString + "plus";
         htmlString = htmlString + '"></span> </button> </div>'
     }
     $("#adnetwork-button-list").html(htmlString);
@@ -62,11 +66,11 @@ function fillAdNetworkList() {
 
 
 function fillPodFileCode() {
-    $('#file-pod').html("source \'https:\/\/github.com\/CocoaPods\/Specs.git\'\r\nplatform :ios, \'8.0\'\r\n\r\nuse_frameworks!\r\n\r\ntarget \'MyAwesomeTarget\' do\r\n#core SDK\r\npod \'"+obj.ad_networks[0].pod+"\', \'~&gt; "+ obj.ad_networks[0].version+"\'\r\n#mediation adapters\n");
+    $('#file-pod').html("source \'https:\/\/github.com\/CocoaPods\/Specs.git\'\r\nplatform :ios, \'8.0\'\r\n\r\nuse_frameworks!\r\n\r\ntarget \'MyAwesomeTarget\' do\r\n#core SDK\r\npod \'"+obj.adNetworks[0].adapterName+"\', \'~&gt; "+ obj.adNetworks[0].podVersion+"\'\r\n#mediation adapters\n");
 
-    for (var i = 1; i < obj.ad_networks.length; i++) {
-        if (obj.ad_networks[i].pod && obj.ad_networks[i].status == true && document.getElementById("file-pod").innerHTML.indexOf(obj.ad_networks[i].pod) == -1) {
-          $('#file-pod').append("pod \'"+ obj.ad_networks[i].pod+"\', \'~&gt; "+obj.ad_networks[i].version +"\'\r\n");
+    for (var i = 1; i < obj.adNetworks.length; i++) {
+        if (obj.adNetworks[i].adapterName && obj.adNetworks[i].status == true && document.getElementById("file-pod").innerHTML.indexOf(obj.adNetworks[i].adapterName) == -1) {
+          $('#file-pod').append("pod \'"+ obj.adNetworks[i].adapterName+"\', \'~&gt; "+obj.adNetworks[i].podVersion +"\'\r\n");
         }
     }
 
@@ -78,18 +82,30 @@ function fillIos14FileCode(){
     var selectedNetworks = [];
     
     //$('#file-ios-14').text("<key>SKAdNetworkItems</key>");    
-    for (var i = 1; i < obj.ad_networks.length; i++) {
-        if(obj.ad_networks[i].status == true){
-            selectedNetworks.push(obj.ad_networks[i]);
-            for(var j = 0 ; j<obj14.length;j++){
-                if(obj14[j].name == obj.ad_networks[i].name){
-                    for(k = 0; k < obj14[j].skAdNetwork.length;k++){
-                        if(!includesUpper(finalValues,obj14[j].skAdNetwork[k])){//if(!finalValues.includes(obj14[j].skAdNetwork[k])){
-                            finalValues.push(obj14[j].skAdNetwork[k]);
-                        }
-                    }
+    // for (var i = 1; i < obj.adNetworks.length; i++) {
+    //     if(obj.adNetworks[i].status == true){
+    //         selectedNetworks.push(obj.adNetworks[i]);
+    //         for(var j = 0 ; j<obj14.length;j++){
+    //             if(obj14[j].displayName == obj.adNetworks[i].displayName){
+    //                 for(k = 0; k < obj14[j].skAdNetwork.length;k++){
+    //                     if(!includesUpper(finalValues,obj14[j].skAdNetwork[k])){//if(!finalValues.includes(obj14[j].skAdNetwork[k])){
+    //                         finalValues.push(obj14[j].skAdNetwork[k]);
+    //                     }
+    //                 }
+    //             }
+    //         }            
+    //     }
+    // }
+
+    //$('#file-ios-14').text("<key>SKAdNetworkItems</key>");    
+    for (var i = 1; i < obj.adNetworks.length; i++) {
+        if(obj.adNetworks[i].status == true){
+            selectedNetworks.push(obj.adNetworks[i]);
+            for(var j = 0; j<obj.adNetworks[i].skadnetworkIDS.length; j++){
+                if(!includesUpper(finalValues,obj.adNetworks[i].skadnetworkIDS[j])){//if(!finalValues.includes(obj14[j].skAdNetwork[k])){
+                    finalValues.push(obj.adNetworks[i].skadnetworkIDS[j]);
                 }
-            }            
+            }          
         }
     }
 
@@ -114,4 +130,12 @@ function includesUpper(arr,item)
         if(arr[i].toUpperCase() == item.toUpperCase()) return true;
     }
     return false;
+}
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
 }
